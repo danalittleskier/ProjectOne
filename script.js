4eddafef656d3f72eb02015948e586c17a6005b9
Bahaar Rokhva <rokhvab@gmail.com>
2019-08-17 20:40:10 -0600

    got all the links hooked up, dropdown is working

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// console.log($('.dropdown-trigger').length);
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.dropdown-trigger');
+//     var instances = M.Dropdown.init(elems, {});
+//   });
+
+$('.dropdown-trigger').dropdown();
