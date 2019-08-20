afae4eba79a272dc0371b135afcb46cac120e251
Nikki Ford <nikkigford@gmail.com>
2019-08-20 12:53:27 -0600

    completed length dropdown

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -21,42 +21,103 @@ $("#search").on("click", function (event) {
     },
     method: "GET"
   }).then(function (response) {
-    // Show table (and column names) if there are more than zero results
-    if (response.trails.length > 0) {
-      $("#results").show();
+    //length dropdown
+    let minLength, maxLength;
+    switch ($("#length").val()) {
+      case "1":
+        minLength = 0;
+        maxLength = 5;
+        break;
+      case "2":
+        minLength = 5;
+        maxLength = 10;
+        break;
+      case "3":
+        minLength = 15;
+        break;
     }
+    let trails = response.trails.filter(function (trail) {
+      if (trail.length >= minLength && trail.length <= maxLength) {
+        return true;
+      }
+    });
+    // Show table (and column names) if there are more than zero results
+    if (trails.length > 0) {
+      $("#results").show();
+      $("#results").empty();
+    } else {
+      $("#results").hide();
+    }
+
+    //add column headers to table dynamically
+    $("#results").append(
+      $(`
+        <tr>
+          <th>Name</th>
+          <th>Difficulty</th>
+          <th>Image</th>
+          <th>Length</th>
+          <th>Summary</th>
+        </tr>  
+      `)
+    );
+
     // Add a row to table for each trail
-    response.trails.forEach(function(trail) {
-        // Creates link tag
-        var trailLink = $("<a>").attr("href", trail.url).text(trail.name);
-        // Creates name column
-        var nameColumn = $("<td>").append(trailLink);
-        // Creates difficulty column
-        var difficultyColumn = $("<td>").text(trail.difficulty);
-        // Creates trail image
-        var trailImage = $("<img>").attr("src", trail.imgSmall);
-        // Creates image column
-        var imageColumn = $("<td>").append(trailImage);
-        // Creates length column
-        var lengthColumn = $("<td>").text(trail.length);
-        // Creates summary column
-        var summaryColumn = $("<td>").text(trail.summary); 
-        // Creates row element and append columns to row
-        var row = $("<tr>").append(nameColumn, difficultyColumn, imageColumn, lengthColumn, summaryColumn);
-        // Append row to table
-        $("#results").append(row);
+    trails.forEach(function (trail) {
+      // Creates link tag
+      // var trailLink = $("<a>").attr("href", trail.url).text(trail.name);
+      // // Creates name column
+      // var nameColumn = $("<td>").append(trailLink);
+      // // Creates difficulty column
+      // var difficultyColumn = $("<td>").text(trail.difficulty);
+      // // Creates trail image
+      // var trailImage = $("<img>").attr("src", trail.imgSmall);
+      // // Creates image column
+      // var imageColumn = $("<td>").append(trailImage);
+      // // Creates length column
+      // var lengthColumn = $("<td>").text(trail.length);
+      // // Creates summary column
+      // var summaryColumn = $("<td>").text(trail.summary); 
+      // // Creates row element and append columns to row
+      // var row = $("<tr>").append(nameColumn, difficultyColumn, imageColumn, lengthColumn, summaryColumn);
+      // // Append row to table
+      // $("#results").append(row);
+
+      //changed above code from JQuery tag building to JS template string
+      $("#results").append(
+        $(`
+            <tr>
+              <td>
+                <a href="${trail.url}">${trail.name}</a>
+              </td>
+              <td>
+                ${trail.difficulty}
+              </td>
+              <td>
+                <img src="${trail.imgSmall}">
+              </td>
+              <td>
+                ${trail.length}
+              </td>
+              <td>
+                ${trail.summary}
+              </td>      
+            </tr>     
+          `)
+      );
 
     })
   });
 });
 
 
+
 //spotify api example
 $("#music-list").on("click", function (event) {
-  
+
   var clientID = "ab78c9bfe2104f2e9e01b86f908541a9";
   var ourProjectURL = encodeURIComponent("https://danalittleskier.github.io/ProjectOne/");
-  var queryURL = "https://accounts.spotify.com/authorize?client_id=" + clientID + "&response_type=code&redirect_uri="+ ourProjectURL +"&scope=user-read-private%20user-read-email"; // &state=34fFs29kd09"
+  var queryURL = "https://accounts.spotify.com/authorize?client_id=" + clientID + "&response_type=code&redirect_uri=" + ourProjectURL + "&scope=user-read-private%20user-read-email"; // &state=34fFs29kd09"
   window.location.href = queryURL;
 
   // console.log(queryURL);
