4fea0b2b3dbfddced38bd14a22a8dc0858906870
DanaLittleSkier <dana.alexandrescu@gmail.com>
2019-08-21 19:48:30 -0600

    Testing localhost instead of github host

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,7 +3,7 @@ var clientID = "ab78c9bfe2104f2e9e01b86f908541a9";
 
 var ourProjectURL = (typeof localURL !== undefined) ? localURL : "https://danalittleskier.github.io/ProjectOne/";
 
-var encodedProjectURL = encodeURIComponent(ourProjectURL);  // "https://danalittleskier.github.io/ProjectOne/");
+var encodedProjectURL = encodeURIComponent(ourProjectURL);
 var spotifyURL = "https://accounts.spotify.com/authorize?client_id=" + clientID + "&response_type=code&redirect_uri=" + encodedProjectURL + "&scope=user-read-private%20user-read-email"; // &state=34fFs29kd09"
 
 // console.log($('.dropdown-trigger').length);
@@ -213,7 +213,7 @@ $(document).ready(function () {
       data: {
         grant_type: "authorization_code",
         code: accessCode,
-        redirect_uri: "http://localhost:3000/",
+        redirect_uri: ourProjectURL,
         client_id: clientID,
         client_secret: "5065d0f119f5424796fbc25fc5346c4c"
       },
