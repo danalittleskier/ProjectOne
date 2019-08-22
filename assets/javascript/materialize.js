361fa9689fd5157ba17363557c3c2c0a37351b09
Bahaar Rokhva <rokhvab@gmail.com>
2019-08-21 20:20:30 -0600

    starting to style the table

--- assets/javascript/materialize.js
+++ assets/javascript/materialize.js
@@ -1,8 +1,8 @@
 
 //trigger for dropdown
 $('.dropdown-trigger').dropdown();
-
 //makes the dropdown select work
 $(document).ready(function () {
   $('select').formSelect();
+  $('#resultsCollapsible').collapsible();
 });
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,3 @@
-var accessCode;
-var clientID = "ab78c9bfe2104f2e9e01b86f908541a9";
-
-var ourProjectURL = (typeof localURL !== undefined) ? localURL : "https://danalittleskier.github.io/ProjectOne/";
-
-var encodedProjectURL = encodeURIComponent(ourProjectURL);
-var spotifyURL = "https://accounts.spotify.com/authorize?client_id=" + clientID + "&response_type=code&redirect_uri=" + encodedProjectURL + "&scope=user-read-private%20user-read-email"; // &state=34fFs29kd09"
-
 // console.log($('.dropdown-trigger').length);
 // document.addEventListener('DOMContentLoaded', function() {
 //     var elems = document.querySelectorAll('.dropdown-trigger');
@@ -15,9 +7,9 @@ var spotifyURL = "https://accounts.spotify.com/authorize?client_id=" + clientID 
 //global variables for color difficulties
 var difficultyMap = {
   green: "easy",
-  greenBlue: "easy to medium",
-  blue: "medium",
-  blueBlack: "medium to difficult",
+  greenBlue: "easy to moderate",
+  blue: "moderate",
+  blueBlack: "moderate to difficult",
   black: "difficult",
 }
 //hides the table until the user clicks on it
@@ -26,159 +18,175 @@ $("#results").hide();
 // on click listener for the serach button
 $("#search").on("click", function (event) {
 
-  // This tells them they need to pick parameters, if they are missing any
-  $("#searchError").empty();
-  let city = $("#city").val();
-  let hasCity = city !== undefined && city !== null && city !== "";
-  if (hasCity === false) {
-    $("#searchError").append("You must provide a city to search.<br>");
-  }
-  let intensity = $("#intensity").val();
-  let hasIntensity = intensity !== undefined && intensity !== null && intensity !== "";
-  if (hasIntensity === false) {
-    $("#searchError").append("You must select a trail intensity.<br>");
-  }
-  let length = $("#length").val();
-  let hasLength = length !== undefined && length !== null && length !== "";
-  if (hasLength === false) {
-    $("#searchError").append("You must select a trail length.<br>");
-  }
-  let duration = $("#duration").val();
-  let hasDuration = duration !== undefined && duration !== null && duration !== "";
-  if (hasDuration === false) {
-    $("#searchError").append("You must select a trail duration.<br>");
-  }
-  let valid = hasCity && hasIntensity && hasLength && hasDuration;
-  if (valid === false) {
-    return;
-  }
+// This tells them they need to pick parameters, if they are missing any
+$("#searchError").empty();
+let city = $("#city").val();
+let hasCity = city !== undefined && city !== null && city !== "";
+if (hasCity === false) {
+  $("#searchError").append("You must provide a city to search.<br>");
+}
+let intensity = $("#intensity").val();
+let hasIntensity = intensity !== undefined && intensity !== null && intensity !== "";
+if (hasIntensity === false) {
+  $("#searchError").append("You must select a trail intensity.<br>");
+}
+let length = $("#length").val();
+let hasLength = length !== undefined && length !== null && length !== "";
+if (hasLength === false) {
+  $("#searchError").append("You must select a trail length.<br>");
+}
+let duration = $("#duration").val();
+let hasDuration = duration !== undefined && duration !== null && duration !== "";
+if (hasDuration === false) {
+  $("#searchError").append("You must select a trail duration.<br>");
+}
+let valid = hasCity && hasIntensity && hasLength && hasDuration;
+if (valid === false) {
+  return;
+}
 
 
-  //getGeocode();
-  var geocodeURL = "https://maps.googleapis.com/maps/api/geocode/json?address=";
-  var geocodeAddress = $("#city").val();
-  var geocodeKey = "&key=AIzaSyBA9Yt7UsjtvyCblUWBK0z6qb42GT8M8ok";
-  var geocodeRequest = geocodeURL + geocodeAddress + geocodeKey;
+//getGeocode();
+var geocodeURL = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+var geocodeAddress = $("#city").val();
+var geocodeKey = "&key=AIzaSyBA9Yt7UsjtvyCblUWBK0z6qb42GT8M8ok";
+var geocodeRequest = geocodeURL + geocodeAddress + geocodeKey;
+$.ajax({
+  url: geocodeRequest,
+  method: "GET"
+}).then(function (response) {
+  let geoReturn = response.results[0];
+  let geoLat = geoReturn.geometry.location.lat;
+  let geoLng = geoReturn.geometry.location.lng;
+  //not sure where to use this yet
+ //let geoAddr = geoReturn.formatted_address;
+
+
+  let queryURL = "https://www.hikingproject.com/data/get-trails";
   $.ajax({
-    url: geocodeRequest,
+    url: queryURL,
+    data: {
+      key: "200562733-d1da5fe78fcd60535046cfa0d06ea12f",
+      lat: geoLat,
+      lon: geoLng,
+      maxDistance: "25",
+    },
     method: "GET"
   }).then(function (response) {
-    let geoReturn = response.results[0];
-    let geoLat = geoReturn.geometry.location.lat;
-    let geoLng = geoReturn.geometry.location.lng;
-    //not sure where to use this yet
-    //let geoAddr = geoReturn.formatted_address;
-
-
-    let queryURL = "https://www.hikingproject.com/data/get-trails";
-    $.ajax({
-      url: queryURL,
-      data: {
-        key: "200562733-d1da5fe78fcd60535046cfa0d06ea12f",
-        lat: geoLat,
-        lon: geoLng,
-        maxDistance: "25",
-      },
-      method: "GET"
-    }).then(function (response) {
-      //intensity dropdown
-      let trails = response.trails.filter(function (trail) {
-        if (trail.difficulty === $("#intensity").val()) {
-          return true;
-        }
-      })
-      //length dropdown. This calls to the API, and gets the trails that fit into the parameters I created(the different miles). 
-      //Instead of a if/else statment, I did a switch statement with the minimum and maximum miles
-      let minLength, maxLength;
-      switch ($("#length").val()) {
-        case "1":
-          minLength = 0;
-          maxLength = 5;
-          break;
-        case "2":
-          minLength = 5;
-          maxLength = 10;
-          break;
-        case "3":
-          minLength = 15;
-          break;
+    //intensity dropdown
+    let trails = response.trails.filter(function(trail){
+      if (trail.difficulty === $("#intensity").val()){
+        return true;
       }
-      trails = trails.filter(function (trail) {
-        if (trail.length >= minLength && trail.length <= maxLength) {
-          return true;
-        }
-      });
-      // Show table (and column names) if there are more than zero results
-      //hides it if there are none that match
-      if (trails.length > 0) {
-        $("#results").show();
-        $("#results").empty();
-      } else {
-        $("#results").hide();
-        $("#searchError").append("No Results Found <br>");
+    })
+    //length dropdown. This calls to the API, and gets the trails that fit into the parameters I created(the different miles). 
+    //Instead of a if/else statment, I did a switch statement with the minimum and maximum miles
+    let minLength, maxLength;
+    switch ($("#length").val()) {
+      case "1":
+        minLength = 0;
+        maxLength = 5;
+        break;
+      case "2":
+        minLength = 5;
+        maxLength = 10;
+        break;
+      case "3":
+        minLength = 15;
+        break;
+    }
+    trails = trails.filter(function (trail) {
+      if (trail.length >= minLength && trail.length <= maxLength) {
+        return true;
       }
-
-      //add column headers to table dynamically
-      //originally had this hard coded into the HTML, but need to add it with JS, because I needed to empty the table, and it would emply the table headers too
-      $("#results").append(
-        $(`
-        <tr>
-          <th>Name</th>
-          <th>Difficulty</th>
-          <th>Image</th>
-          <th>Length</th>
-          <th>Summary</th>
-        </tr>  
-      `)
-      );
-
-      // Add a row to table for each trail
-      trails.forEach(function (trail) {
-        // Creates link tag
-        // var trailLink = $("<a>").attr("href", trail.url).text(trail.name);
-        // // Creates name column
-        // var nameColumn = $("<td>").append(trailLink);
-        // // Creates difficulty column
-        // var difficultyColumn = $("<td>").text(trail.difficulty);
-        // // Creates trail image
-        // var trailImage = $("<img>").attr("src", trail.imgSmall);
-        // // Creates image column
-        // var imageColumn = $("<td>").append(trailImage);
-        // // Creates length column
-        // var lengthColumn = $("<td>").text(trail.length);
-        // // Creates summary column
-        // var summaryColumn = $("<td>").text(trail.summary); 
-        // // Creates row element and append columns to row
-        // var row = $("<tr>").append(nameColumn, difficultyColumn, imageColumn, lengthColumn, summaryColumn);
-        // // Append row to table
-        // $("#results").append(row);
-
-        //changed above code from JQuery tag building to JS template string
-        $("#results").append(
-          $(`
-            <tr>
-              <td>
-                <a href="${trail.url}">${trail.name}</a>
-              </td>
-              <td>
-                ${difficultyMap[trail.difficulty]}
-              </td>
-              <td>
-                <img src="${trail.imgSmall}">
-              </td>
-              <td>
-                ${trail.length}
-              </td>
-              <td>
-                ${trail.summary}
-              </td>      
-            </tr>     
-          `)
-        );
-
-      })
     });
+    // Show table (and column names) if there are more than zero results
+    //hides it if there are none that match
+    if (trails.length > 0) {
+      $("#results").show();
+      $("#results").empty();
+    } else {
+      $("#results").hide();
+      $("#searchError").append("No Results Found <br>");
+    }
+
+    //add column headers to table dynamically
+    //originally had this hard coded into the HTML, but need to add it with JS, because I needed to empty the table, and it would emply the table headers too
+    // $("#results").append(
+    //   $(`
+    //   <tr>
+    //     <th>Name</th>
+    //     <th>Difficulty</th>
+    //     <th>Image</th>
+    //     <th>Length</th>
+    //     <th>Summary</th>
+    //   </tr>  
+    // `)
+    // );
+
+    // Add a row to table for each trail
+    trails.forEach(function (trail) {
+      // Creates link tag
+      // var trailLink = $("<a>").attr("href", trail.url).text(trail.name);
+      // // Creates name column
+      // var nameColumn = $("<td>").append(trailLink);
+      // // Creates difficulty column
+      // var difficultyColumn = $("<td>").text(trail.difficulty);
+      // // Creates trail image
+      // var trailImage = $("<img>").attr("src", trail.imgSmall);
+      // // Creates image column
+      // var imageColumn = $("<td>").append(trailImage);
+      // // Creates length column
+      // var lengthColumn = $("<td>").text(trail.length);
+      // // Creates summary column
+      // var summaryColumn = $("<td>").text(trail.summary); 
+      // // Creates row element and append columns to row
+      // var row = $("<tr>").append(nameColumn, difficultyColumn, imageColumn, lengthColumn, summaryColumn);
+      // // Append row to table
+      // $("#results").append(row);
+
+      $("#resultsCollapsible").append(`
+        <li>
+          <div class="collapsible-header">
+            <span style="font-weight: bold;">
+              <a href="${trail.url}">
+                ${trail.name}
+              </a>
+            </span> - ${difficultyMap[trail.difficulty]} | ${trail.length} mi
+          </div>
+          <div class="collapsible-body flex">
+            <img src="${trail.imgSmall}">
+            <p>${trail.summary}</p>
+          </div>
+        </li>
+      `);
+
+      //changed above code from JQuery tag building to JS template string
+      // $("#resultsTable").append(
+      //   $(`
+      //     <tr>
+      //       <td>
+      //         <a href="${trail.url}">${trail.name}</a>
+      //       </td>
+      //       <td>
+      //         ${difficultyMap[trail.difficulty]}
+      //       </td>
+      //       <td>
+      //         <img src="${trail.imgSmall}">
+      //       </td>
+      //       <td>
+      //         ${trail.length}
+      //       </td>
+      //       <td>
+      //         ${trail.summary}
+      //       </td>      
+      //     </tr>     
+      //   `)
+      // );
+
+    })
   });
+});
 });
 
 
@@ -186,43 +194,17 @@ $("#search").on("click", function (event) {
 //spotify api example
 $("#music-list").on("click", function (event) {
 
+var clientID = "ab78c9bfe2104f2e9e01b86f908541a9";
+var ourProjectURL = encodeURIComponent("https://danalittleskier.github.io/ProjectOne/");
+var queryURL = "https://accounts.spotify.com/authorize?client_id=" + clientID + "&response_type=code&redirect_uri=" + ourProjectURL + "&scope=user-read-private%20user-read-email"; // &state=34fFs29kd09"
 
-  window.location.href = spotifyURL;
+window.location.href = queryURL;
 
-
-
-  // console.log(queryURL);
-  // $.ajax({
-  //   url: queryURL,
-  //   method: "GET"
-  // }).then(function (response) {
-  //   console.log(response);
-  //   })
+// console.log(queryURL);
+// $.ajax({
+//   url: queryURL,
+//   method: "GET"
+// }).then(function (response) {
+//   console.log(response);
+//   })
 });
-
-$(document).ready(function () {
-  var urlParams = new URLSearchParams(window.location.search);
-  accessCode = urlParams.get('code');
-  var entries = urlParams.entries();
-  console.log(accessCode);
-
-  if (accessCode) {
-    var spotifyPostURL = "https://accounts.spotify.com/api/token";
-    $.ajax({
-      url: spotifyPostURL,
-      data: {
-        grant_type: "authorization_code",
-        code: accessCode,
-        redirect_uri: ourProjectURL,
-        client_id: clientID,
-        client_secret: "5065d0f119f5424796fbc25fc5346c4c"
-      },
-      method: "POST"
-    }).then(function (response) {
-      console.log(response);
-    })
-  }
-
-
-});
-
