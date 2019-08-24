13410e415fbdf1c46c4518859fb3b922912f6efa
Bahaar Rokhva <rokhvab@gmail.com>
2019-08-24 11:45:26 -0600

    got carosel working

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -188,8 +188,8 @@ $("#search").on("click", function (event) {
         </li>
       `);
 
-      $(".container").append(`
-      <div class="carousel carousel-slider center">
+      $(".carousel").append(`
+
         <div class="carousel-fixed-item center">
           <a class="btn waves-effect white grey-text darken-text-2">button</a>
         </div>
@@ -197,19 +197,6 @@ $("#search").on("click", function (event) {
           <h2>First Panel</h2>
           <p class="white-text">This is your first panel</p>
         </div>
-        <div class="carousel-item amber white-text" href="#two!">
-          <h2>Second Panel</h2>
-          <p class="white-text">This is your second panel</p>
-        </div>
-        <div class="carousel-item green white-text" href="#three!">
-          <h2>Third Panel</h2>
-          <p class="white-text">This is your third panel</p>
-        </div>
-        <div class="carousel-item blue white-text" href="#four!">
-          <h2>Fourth Panel</h2>
-          <p class="white-text">This is your fourth panel</p>
-        </div>
-    </div>
     `);
 
    
