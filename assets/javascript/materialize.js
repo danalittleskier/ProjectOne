93b4ffa040c1e3bea3010f301053deda5517b705
Bahaar Rokhva <rokhvab@gmail.com>
2019-08-26 00:24:18 -0600

    final styling

--- assets/javascript/materialize.js
+++ assets/javascript/materialize.js
@@ -12,22 +12,17 @@ $(document).ready(function(){
   $('.collapsible').collapsible();
 });
 
-$(function(){
-  var shrinkHeader = 300;
-   $(window).scroll(function() {
-     var scroll = getCurrentScroll();
-       if ( scroll >= shrinkHeader ) {
-            $('.header').addClass('shrink');
-         }
-         else {
-             $('.header').removeClass('shrink');
-         }
-   });
-   
-function getCurrentScroll() {
-     return window.pageYOffset || document.documentElement.scrollTop;
-     }
- });
+$(document).on("scroll", function(){
+  if
+    ($(document).scrollTop() > 100){
+    $("header").addClass("shrink");
+  }
+  else
+  {
+    $("header").removeClass("shrink");
+  }
+});
+
 
 
 
